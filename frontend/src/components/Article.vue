46264a03e0e259ba2787ba7ace99a15f7864fe78
zw3413 <zhangweicalm@qq.com>
2020-06-19 12:31:07 +0800

    modified some article styles

--- frontend/src/components/Article.vue
+++ frontend/src/components/Article.vue
@@ -1,17 +1,19 @@
 <template>
   <div style>
-  <div style="width:100%;height:20px;text-align:center">
-      <p><a href="#" @click="loadNextPage()">load more</a></p>
-    </div>
-    <el-table :data="tableData" style="width: 100%">
-      <el-table-column prop="media_content" label="media_content" width="200px">
+    <!-- <div style="width:100%;height:20px;text-align:center">
+      <p>
+        <a href="#" @click="loadNextPage()">load more</a>
+      </p>
+    </div> -->
+    <el-table :data="tableData" style="width: 100%" id="media">
+      <el-table-column prop="media_content" label="media_content" width="150px">
         <template slot-scope="scope">
           <ul>
             <li v-for="media in scope.row.media_content" :key="media.url">
               <img
                 :src="media.url"
-                :height="media.height"
-                :width="media.width"
+                
+                width="145px"
                 :alt="scope.row.media_text"
               />
             </li>
@@ -27,25 +29,30 @@
       <el-table-column label="title">
         <template slot-scope="scope">
           <p>
-            {{scope.row.title}}
+            <a @click="showArticle(scope.row)">{{scope.row.title}}</a>
             &nbsp;&nbsp;&nbsp;&nbsp;
-            <a
+            <br/>
+            <!-- <a
               v-if="scope.row.content && scope.row.content != ''"
               @click="showArticle(scope.row)"
             >正文</a>
-            &nbsp;&nbsp;&nbsp;&nbsp;
-            <a v-bind:href="scope.row.link" v-bind:title="scope.row.link" target="_blank">原文链接</a>
+            &nbsp;&nbsp;&nbsp;&nbsp; -->
+            <!-- <a
+              v-bind:href="scope.row.link"
+              v-bind:title="scope.row.link"
+              target="_blank"
+            >原文链接</a> -->
           </p>
           <p>
             <span>{{scope.row.pub_date}}</span>
           </p>
-          <p>
+          <!-- <p>
             <a
               :href="scope.row.source_url"
               :title="scope.row.source_url"
               target="_blank"
             >{{scope.row.source}}</a>
-          </p>
+          </p> -->
         </template>
       </el-table-column>
       <!-- <el-table-column prop="summary" label="summary"></el-table-column>
@@ -68,14 +75,15 @@
       <!-- <el-table-column prop="profile_name" label="profile_name"></el-table-column> -->
       <!-- <el-table-column prop="update_time" label="update_time"></el-table-column> -->
     </el-table>
-    
-    <el-dialog 
-    title="正文" 
-    :visible.sync="dialogVisible"  
+
+    <el-dialog :title="article.title" 
+    :visible.sync="dialogVisible" 
     width="90%"
-  
+    top="2vh"
+    :show-close= "modalShowClose"
+    :fullscreen="modalFullScreen"
     >
-      <h2 v-html="article.title"></h2>
+      <!-- <h2 v-html="article.title"></h2> -->
       <div v-html="article.content"></div>
       <ul>
         <li v-for="translation in translations" :key="translation.guid">
@@ -87,71 +95,115 @@
       <!-- <div slot="footer" class="dialog-footer">
         <el-button @click.native="dialogVisible = false">取消</el-button>
         <el-button type="primary" :loading="addLoading">提交</el-button>
-      </div> -->
+      </div>-->
     </el-dialog>
   </div>
 </template>
 <script>
 //import {urlEncode} from '../utils/common_util'
 import { getList } from "../api/article";
-import {getTranslationByGuid} from "../api/article";
+import { getTranslationByGuid } from "../api/article";
 export default {
   data() {
     return {
-      article:{},
-      translations:{},
-      params: {page:0},
+      article: {},
+      translations: {},
+      params: { page: 0 },
       tableData: [],
       dialogVisible: false, //模态框是否显示
-      addLoading: false //是否显示loading
+      addLoading: false, //是否显示loading
+      lastScrollTop:-1,
+      modalShowClose:true,
+      modalFullScreen:false,
     };
   },
   methods: {
-    loadNextPage:function() {
-      this.fetchList()
+    onScroll:function() {
+      this.has_log = 1;
+      let innerHeight = document.querySelector("#media").clientHeight;
+      let outerHeight = document.documentElement.clientHeight;
+      if(this.lastScrollTop>-1){
+        document.documentElement.scrollTop=this.lastScrollTop;
+        this.lastScrollTop=-1;
+      }
+      let scrollTop =
+        document.documentElement.scrollTop ||
+        document.body.scrollTop ||
+        window.pageYOffset;
+
+
+      console.log(innerHeight + " " + outerHeight + " " + scrollTop);
+
+      if ((outerHeight + scrollTop) == (innerHeight)) {
+        if (this.no_data === true) {
+          this.has_log = 2;
+          return false;
+        }
+        this.loadNextPage();
+      }
+
+      if (scrollTop <= 0 && typeof(this.params) != "undefined") {
+        console.log(this.params.page)
+        this.params.page = 0;
+        this.refreshPage();
+      }
+    },
+    loadNextPage: function() {
+      this.fetchList(true);
+    },
+    refreshPage:function(){
+      this.fetchList();
     },
     opendialog: function() {
       //代开模态框
       this.dialogVisible = false;
     },
-    fetchList: function() {
+    fetchList: function(append) {
       this.params.page++;
       getList(this.params).then(data => {
-        if(typeof(data.results) =="undefined" ){
-            alert('没有更多的内容')
-        }else{
-        //console.log(data)
-        data.results.forEach(function(v) {
-          //v.title=url
-          v.media_content = eval(v.media_content);    
-          if (v.media_content) {
-            v.media_content.forEach(c => {
-              if (typeof c.width == "undefined") c.width = "130";
-              if (typeof c.height == "undefined") c.height = "90";
-            });
+        if (typeof data.results == "undefined") {
+          alert("没有更多的内容");
+        } else {
+          //console.log(data)
+          data.results.forEach(function(v) {
+            //v.title=url
+            v.media_content = eval(v.media_content);
+            if (v.media_content) {
+              v.media_content.forEach(c => {
+                if (typeof c.width == "undefined") c.width = "130";
+                if (typeof c.height == "undefined") c.height = "90";
+              });
+            }
+          });
+          if(append){
+            //this.tableData = data.results.concat(this.tableData);
+            this.lastScrollTop =
+                document.documentElement.scrollTop ||
+                document.body.scrollTop ||
+                window.pageYOffset;
+            this.tableData= this.tableData.concat(data.results)
+          }else{
+            this.tableData = data.results
           }
-        });
-        this.tableData = data.results.concat(this.tableData);
-        console.log(this.tableData)
+          console.log(this.tableData);
         }
       });
     },
     showArticle: function(row) {
+      this.article = row;
 
-      this.article=row
-
-      getTranslationByGuid({guid:row.guid}).then(data=>{
+      getTranslationByGuid({ guid: row.guid }).then(data => {
         //  if(typeof(data.results) =="undefined" ){
         //     alert('没有更多的内容')
-        // }else{   
+        // }else{
         //   this.translations = data.results;
         //   console.log(this.tableData)
         // }
-        this.translations=data;
-        console.log(this.translations)
-      })
+        this.translations = data;
+        console.log(this.translations);
+      });
 
-      this.dialogVisible=true
+      this.dialogVisible = true;
       // this.$alert(content, "正文", {
       //   dangerouslyUseHTMLString: true,
       //   center: true,
@@ -163,6 +215,7 @@ export default {
   computed: {},
   mounted: function() {
     this.fetchList();
+     window.addEventListener('scroll', this.onScroll);
   }
 };
 </script>
@@ -174,9 +227,18 @@ li {
   width: 90% !important;
 }
 div {
-  padding:0 !important;
+  padding: 0 !important;
 }
-.el-dialog button{
+
+body, #app, ul, li {
+  padding:0;
+  margin:0;
+  border:0;
+}
+thead{
   display:none
+}
+p{
+  margin:3px 0
 }
 </style>
